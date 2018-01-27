1ea78eb3dd2be4974a3e9acaeef39db8b267c637
yuanqijie <yuanqijie>
2018-01-27 21:38:12 +0800

    second commit

--- network_reconfigration.py
+++ network_reconfigration.py
@@ -58,8 +58,7 @@ def milp_sdn_routing(res_bw, flows, edge_info, path_num, flow_require):
         model += total_used_bd[edge] <= res_bw[edge]
 
     # objection
-    # cost_list = defaultdict(int)
-    cost_list = []
+    cost_list = defaultdict(int)
     total_used = {}
     for edge in edges:
         total_used[edge] = 0
@@ -67,15 +66,15 @@ def milp_sdn_routing(res_bw, flows, edge_info, path_num, flow_require):
             total_used[edge] += sum(flow_require[flow] * y[(flow, i)] *
                                        edge_info[edge][flow][i] for i in xrange(path_num))
         used_bd = 10 - (res_bw[edge] - total_used[edge])
-        cost_list.append(link_cost(used_bd))
+
         # cost_list[edge] = int(link_cost(used_bd))
         # cost_list[edge] = used_bd if 0 <= used_bd < 10.0/3 else 3*used_bd if 10.0/3 <= used_bd < 20.0/3 \
         #     else 10 * used_bd - 160.0/3 if 20.0/3 <= used_bd < 9 else 70 * used_bd - 1780.0/3 if \
         #     9 <= used_bd < 10 else 700
-        # cost_list[edge] = used_bd
+        cost_list[edge] = used_bd
 
     # model += pulp.lpSum(float(cost_list[edge]) for edge in edges), 'minimize the link cost'
-    model += pulp.lpSum(cost_list), 'minimize the link cost'
+    model += pulp.lpSum(cost_list.values()), 'minimize the link cost'
 
     model.solve()
     status = pulp.LpStatus[model.status]
