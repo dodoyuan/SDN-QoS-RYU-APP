298cc8855efd9bba04ea5695b40feb144965a529
yuanqijie <yuanqijie>
2018-01-28 11:11:19 +0800

    second commit

--- network_reconfigration.py
+++ network_reconfigration.py
@@ -44,6 +44,7 @@ def milp_sdn_routing(res_bw, flows, edge_info, path_num, flow_require):
     model = pulp.LpProblem("link load balance", pulp.LpMinimize)
     y = pulp.LpVariable.dicts("choose a path", [(flow, path_index) for flow in flows
             for path_index in xrange(path_num)], 0, 1, cat='Binary')
+    z = pulp.LpVariable('z',lowBound=0,cat='Continuous')
 
     # constrains 1
     for flow in flows:
@@ -56,11 +57,12 @@ def milp_sdn_routing(res_bw, flows, edge_info, path_num, flow_require):
         for flow in flows:
             total_used_bd[edge] += sum(flow_require[flow] * y[(flow, i)] *
                                        edge_info[edge][flow][i] for i in xrange(path_num))
+
         model += total_used_bd[edge] <= res_bw[edge]
 
     # objection
     # cost_list = defaultdict(int)
-    cost_list = []
+    # cost_list = []
     total_used = {}
     for edge in edges:
         total_used[edge] = 0
@@ -68,7 +70,8 @@ def milp_sdn_routing(res_bw, flows, edge_info, path_num, flow_require):
             total_used[edge] += sum(flow_require[flow] * y[(flow, i)] *
                                        edge_info[edge][flow][i] for i in xrange(path_num))
         used_bd = 10 - (res_bw[edge] - total_used[edge])
-        cost_list.append(used_bd)
+        model += z >= used_bd
+        # cost_list.append(used_bd)
 
         # cost_list[edge] = int(link_cost(used_bd))
         # cost_list[edge] = used_bd if 0 <= used_bd < 10.0/3 else 3*used_bd if 10.0/3 <= used_bd < 20.0/3 \
@@ -80,7 +83,7 @@ def milp_sdn_routing(res_bw, flows, edge_info, path_num, flow_require):
 
     # model += pulp.lpSum(float(cost_list[edge]) for edge in edges), 'minimize the link cost'
     # model += pulp.lpSum(cost_list), 'minimize the link cost'
-    model += max(cost_list[i] for i in xrange(len(cost_list))), 'minimize the link cost'
+    model += max(z), 'minimize the link cost'
 
     model.solve()
     status = pulp.LpStatus[model.status]
